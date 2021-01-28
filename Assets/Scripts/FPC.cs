a7fbe5d74987582feb339b9241aa591b5565422c
zpallin <zpallin@gmail.com>
2021-01-27 22:09:47 -0800

    Add character controller and player object with player input system
    - Profiles/PlayerController which sets the action maps
    - Scripts/Runtime/Input/PlayerController which is the C# output
    - FPC which is the actual player script that uses and is triggered by
      player controller

---
+++ Assets/Scripts/FPC.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+//https://www.youtube.com/watch?v=vbILVirFV3A
+public class FPC : MonoBehaviour
+{
+    [SerializeField] Transform cam;
+    [SerializeField] float speed = 10f;
+    [SerializeField] float angularSpeed;
+
+    CharacterController Controller;
+    Vector2 Move;
+    float MouseX;
+    float MouseY;
+
+    void Awake()
+    {
+        Controller = GetComponent<CharacterController>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Move = context.ReadValue<Vector2>();
+        Debug.Log(Move);
+    }
+
+    public void OnRotationX(InputAction.CallbackContext context)
+    {
+        MouseX = context.ReadValue<float>();
+    }
+
+    public void OnRotationY(InputAction.CallbackContext context)
+    {
+        MouseY = context.ReadValue<float>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaTime = Time.deltaTime;
+        Vector3 newMove = new Vector3(Move.x * speed * deltaTime, 0f, Move.y * speed * deltaTime);
+        Controller.Move(newMove);
+        //cam.transform.Rotate(MouseX, MouseY, 0); // this doesnt work
+    }
+}
---
+++ Assets/Scripts/Runtime/Input/PlayerController.cs
@@ -0,0 +1,234 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Profiles/PlayerController.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerController : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerController()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerController"",
+    ""maps"": [
+        {
+            ""name"": ""GamePlay"",
+            ""id"": ""666b97c0-a2c3-471b-b0c8-f152b3065b77"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""675f6fe6-5342-48e6-931c-24fb1ffe6627"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""RotationX"",
+                    ""type"": ""Value"",
+                    ""id"": ""6157fe36-fc5f-4c8c-8f8d-c498813cf18f"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""RotationY"",
+                    ""type"": ""Value"",
+                    ""id"": ""6dfacb8f-2271-491c-a3db-c2e942a02838"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""Direction"",
+                    ""id"": ""cb54ff83-08a7-42d9-94fe-920feb7a4309"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""31f33a1d-1ebb-4fb7-affa-506f1536b7a9"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""56c3aea9-5c4d-4dc5-a53d-318d9ae8569f"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""60148ec8-ff57-407c-bfeb-ca7f4d32e878"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""b128bbc0-1d18-49f6-92c1-4553e07fc7da"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""8c9a6631-9886-491b-bfc5-68f495d130b6"",
+                    ""path"": ""<Mouse>/delta/x"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""RotationX"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""741c62c8-f906-4391-90fd-571468a3184b"",
+                    ""path"": ""<Mouse>/delta/y"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""RotationY"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // GamePlay
+        m_GamePlay = asset.FindActionMap("GamePlay", throwIfNotFound: true);
+        m_GamePlay_Movement = m_GamePlay.FindAction("Movement", throwIfNotFound: true);
+        m_GamePlay_RotationX = m_GamePlay.FindAction("RotationX", throwIfNotFound: true);
+        m_GamePlay_RotationY = m_GamePlay.FindAction("RotationY", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // GamePlay
+    private readonly InputActionMap m_GamePlay;
+    private IGamePlayActions m_GamePlayActionsCallbackInterface;
+    private readonly InputAction m_GamePlay_Movement;
+    private readonly InputAction m_GamePlay_RotationX;
+    private readonly InputAction m_GamePlay_RotationY;
+    public struct GamePlayActions
+    {
+        private @PlayerController m_Wrapper;
+        public GamePlayActions(@PlayerController wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_GamePlay_Movement;
+        public InputAction @RotationX => m_Wrapper.m_GamePlay_RotationX;
+        public InputAction @RotationY => m_Wrapper.m_GamePlay_RotationY;
+        public InputActionMap Get() { return m_Wrapper.m_GamePlay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GamePlayActions set) { return set.Get(); }
+        public void SetCallbacks(IGamePlayActions instance)
+        {
+            if (m_Wrapper.m_GamePlayActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMovement;
+                @RotationX.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnRotationX;
+                @RotationX.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnRotationX;
+                @RotationX.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnRotationX;
+                @RotationY.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnRotationY;
+                @RotationY.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnRotationY;
+                @RotationY.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnRotationY;
+            }
+            m_Wrapper.m_GamePlayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @RotationX.started += instance.OnRotationX;
+                @RotationX.performed += instance.OnRotationX;
+                @RotationX.canceled += instance.OnRotationX;
+                @RotationY.started += instance.OnRotationY;
+                @RotationY.performed += instance.OnRotationY;
+                @RotationY.canceled += instance.OnRotationY;
+            }
+        }
+    }
+    public GamePlayActions @GamePlay => new GamePlayActions(this);
+    public interface IGamePlayActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnRotationX(InputAction.CallbackContext context);
+        void OnRotationY(InputAction.CallbackContext context);
+    }
+}
